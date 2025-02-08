882f1f755af011d4311ae80fdad104e99dbbbacc
Alick Lazare <alick_lazare97@live.com>
2025-02-07 22:13:48 -0500

    - homepage, paintingpages

---
+++ src/app/painting/[year]/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+const imgs = [
+    '/painting.png',
+    '/painting.png',
+    '/painting.png',
+    '/painting.png',
+    '/painting.png',
+    '/painting.png'
+]
+
+export default function PaintingsFromYear({ params }: { params: { year: string }}) {
+    
+    return (
+    <div className=" text-3xl flex gap-6 items-center min-h-screen justify-start md:px-10">
+        <span className="border border-white p-6 text-3xl lg:text-5xl">
+            {params.year}
+        </span>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 3xl:grid-cols-4 gap-6">
+        {imgs.map((img, indx) => (
+            <a className=" relative w-60 h-60 border border-white p-10 cursor-pointer hover:scale-105" key = {`${img} ${indx}`} href = {img}>
+                <Image fill className="object-cover"  src={img} alt="test img" />
+            </a>
+            ))}
+        </div>
+    </div>
+    )
+}
---
+++ src/app/painting/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+const years = [
+    {title: "2023", url: "/painting/2023"},
+    {title: "2024", url: "/painting/2024"},
+    {title: "2025", url: "/painting/2025"},
+]
+export default function PaintingLanding() {
+
+    return(
+        <div className=" text-3xl flex gap-6 items-center min-h-screen justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-6">
+            {years.map((year) => (
+                <Link className="border border-white p-10 cursor-pointer hover:scale-105 min-size-9xl text-3xl xl:text-9xl" key = {year.url} href = {year.url}>
+                    {year.title}
+                </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
